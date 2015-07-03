53cb22357ebaf24d25ea5046f48f63ed99ead943
Kuang Yuan <kngyuan@gmail.com>
2015-07-02 22:38:40 -0700

    Initial commit

---
+++ c1/src/jvm/storm/WordCountBolt.java
@@ -0,0 +1,43 @@
+package storm;
+
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.testing.TestWordSpout;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+import backtype.storm.utils.Utils;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class WordCountBolt extends BaseRichBolt {
+  private OutputCollector collector;
+  private HashMap<String, Long> counts = null;
+
+  public void prepare(Map config, TopologyContext context, OutputCollector collector) {
+    this.collector = collector;
+    this.counts = new HashMap<String, Long>() ;
+  }
+
+  public void execute(Tuple tuple) {
+    String word = tuple.getStringByField("word");
+    Long count = this.counts.get(word);
+    if(count == null) {
+      count = 0L;
+    }
+    ++count;
+    this.counts.put(word, count);
+    this.collector.emit(new Values(word, count));
+  }
+
+  public void declareOutputFields(OutputFieldsDeclarer declarer) {
+    declarer.declare(new Fields("word", "count"));
+  }
+}
